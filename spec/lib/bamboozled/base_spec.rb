30e5cac2adff997343f9dad8db6aadb0733e957f
Alan Lamas <alan12394@hotmail.com>
2023-03-29 15:02:47 -0300

    fixed test adding api_url parameter

--- spec/lib/bamboozled/base_spec.rb
+++ spec/lib/bamboozled/base_spec.rb
@@ -1,26 +1,28 @@
 require "spec_helper"
 
 RSpec.describe "Bamboozled::Base" do
-  let(:base) { Bamboozled::Base.new(subdomain: "x", api_key: "x", httparty_options: {log_format: :curl}) }
+  let(:base) do
+    Bamboozled::Base.new(subdomain: "x", api_key: "x", httparty_options: {log_format: :curl}, api_base: 'newapi.com/url')
+  end
 
   it "passes HTTParty options to Bamboozled::API::Employee constructor" do
-    expect(Bamboozled::API::Employee).to receive(:new).with("x", "x", { log_format: :curl })
+    expect(Bamboozled::API::Employee).to receive(:new).with("x", "x", { log_format: :curl }, 'newapi.com/url')
     base.employee
   end
   it "passes HTTParty options to Bamboozled::API::Report constructor" do
-    expect(Bamboozled::API::Report).to receive(:new).with("x", "x", { log_format: :curl })
+    expect(Bamboozled::API::Report).to receive(:new).with("x", "x", { log_format: :curl }, 'newapi.com/url')
     base.report
   end
   it "passes HTTParty options to Bamboozled::API::Meta constructor" do
-    expect(Bamboozled::API::Meta).to receive(:new).with("x", "x", { log_format: :curl })
+    expect(Bamboozled::API::Meta).to receive(:new).with("x", "x", { log_format: :curl }, 'newapi.com/url')
     base.meta
   end
   it "passes HTTParty options to Bamboozled::API::TimeOff constructor" do
-    expect(Bamboozled::API::TimeOff).to receive(:new).with("x", "x", { log_format: :curl })
+    expect(Bamboozled::API::TimeOff).to receive(:new).with("x", "x", { log_format: :curl }, 'newapi.com/url')
     base.time_off
   end
   it "passes HTTParty options to Bamboozled::API::TimeTracking constructor" do
-    expect(Bamboozled::API::TimeTracking).to receive(:new).with("x", "x", { log_format: :curl })
+    expect(Bamboozled::API::TimeTracking).to receive(:new).with("x", "x", { log_format: :curl }, 'newapi.com/url')
     base.time_tracking
   end
 end
