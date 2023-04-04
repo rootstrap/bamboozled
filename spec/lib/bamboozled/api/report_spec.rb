0668a08ba5877f502fe44e6e639ed16e6d07a3de
Agostina Camacho <agostinac.dev@gmail.com>
2023-04-04 11:10:25 -0300

    Add tests for filtered custom report

--- spec/lib/bamboozled/api/report_spec.rb
+++ spec/lib/bamboozled/api/report_spec.rb
@@ -5,13 +5,28 @@ RSpec.describe "Reports" do
     @client = Bamboozled.client(subdomain: "x", api_key: "x")
   end
 
-  it "Creates a custom report" do
-    response = File.new("spec/fixtures/custom_report.json")
-    stub_request(:any, /.*api\.bamboohr\.com.*/).to_return(response)
+  context "When there are no date filters" do
+    it "Creates a custom report" do
+      response = File.new("spec/fixtures/custom_report.json")
+      stub_request(:any, /.*api\.bamboohr\.com.*/).to_return(response)
+  
+      employees = @client.report.custom(%w[bestEmail employeeNumber birthday])
+  
+      expect(employees).to be_an Array
+      expect(employees.size).to eq 2
+    end
+  end
 
-    employees = @client.report.custom(%w[bestEmail employeeNumber birthday])
-
-    expect(employees).to be_an Array
-    expect(employees.size).to eq 2
+  context "When there are lsat changed date filters" do
+    it "Creates a filtered custom report" do
+      response = File.new("spec/fixtures/custom_report_filtered.json")
+      stub_request(:any, /.*api\.bamboohr\.com.*/).to_return(response)
+  
+      employees = @client.report.custom(%w[bestEmail employeeNumber birthday], true,
+                                        "2023-04-03T00:00:00Z")
+  
+      expect(employees).to be_an Array
+      expect(employees.size).to eq 1
+    end
   end
 end
